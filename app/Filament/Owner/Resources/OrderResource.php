07ec7af6a086918049c6e501ceeffaf5409edfc2
fresh talent <218934637+jovank9399@users.noreply.github.com>
2026-01-22 06:40:10 +0100

    Added order resource

---
+++ app/Filament/Owner/Resources/OrderResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Owner\Resources;
+
+use App\Filament\Owner\Resources\OrderResource\Pages;
+use App\Filament\Owner\Resources\OrderResource\RelationManagers;
+use App\Models\Order;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->label('Order #')->sortable(),
+                Tables\Columns\TextColumn::make('shop.name')->label('Shop'),
+                Tables\Columns\TextColumn::make('user.name')->label('Customer'),
+                Tables\Columns\BadgeColumn::make('status'),
+                Tables\Columns\BadgeColumn::make('payment_status'),
+                Tables\Columns\TextColumn::make('grand_total')->money('inr', divideBy: 1),
+                Tables\Columns\TextColumn::make('created_at')->dateTime()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+            ])
+            ->bulkActions([]);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->whereIn('shop_id', auth()->user()->shops()->pluck('id'));
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'view' => Pages\ViewOrder::route('/{record}'),
+        ];
+    }
+}
