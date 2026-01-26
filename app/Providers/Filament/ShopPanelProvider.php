b8b374e10161ed101dd653f79c5b2e56366f42a1
fresh talent <218934637+jovank9399@users.noreply.github.com>
2026-01-26 16:26:44 +0100

    Update ShopPanelProvider.php

--- app/Providers/Filament/ShopPanelProvider.php
+++ app/Providers/Filament/ShopPanelProvider.php
@@ -35,7 +35,6 @@ class ShopPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Owner/Widgets'), for: 'App\\Filament\\Owner\\Widgets')
             ->widgets([
                 \App\Filament\Owner\Widgets\StatsOverview::class,
                 \App\Filament\Owner\Widgets\RecentOrdersWidget::class,
