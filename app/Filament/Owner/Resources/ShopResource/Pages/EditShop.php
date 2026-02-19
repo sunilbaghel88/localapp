3e8c626e2b87f7572ad0935a5ef56f88fb0826d3
fresh talent <218934637+jovank9399@users.noreply.github.com>
2026-02-19 06:09:38 +0100

    Update EditShop.php

--- app/Filament/Owner/Resources/ShopResource/Pages/EditShop.php
+++ app/Filament/Owner/Resources/ShopResource/Pages/EditShop.php
@@ -13,7 +13,7 @@ class EditShop extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            
         ];
     }
 }
